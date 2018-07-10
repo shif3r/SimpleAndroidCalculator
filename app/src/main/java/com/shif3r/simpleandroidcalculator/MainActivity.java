fb00e55031d00496853b8e36e2aee5b9c1d3e79a
Viktor Rodionov <denomit07@gmail.com>
2018-07-10 15:37:32 +0600

    Update #2
    Fixing interface for using this app on devices with different screen resolutions

--- app/src/main/java/com/shif3r/simpleandroidcalculator/MainActivity.java
+++ app/src/main/java/com/shif3r/simpleandroidcalculator/MainActivity.java
@@ -2,9 +2,10 @@ package com.shif3r.simpleandroidcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1,button2,button3,button4,button5,button6,button7,button8,button9,button0,buttonPlus,buttonMinus,buttonDivide,buttonMultiply,resultButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
